cd5df97af52e784304a0c4f6eae00a4550ddf7aa
Your Name <you@example.com>
2020-04-06 12:07:47 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/dist"
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'file-loader'
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin(["static/"])
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 30000,
+        inline: true,
+        open: true,
+        writeToDisk: true
+    }
+};
